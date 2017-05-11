216c765a2ee46a06d388150549fe47a00384285a
cc <guangcan_cc@163.com>
2017-05-11 08:47:44 +0800

    删除 再次提交...

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+	public static void main(String[] args){
+
+		System.out.println("hello world");
+		System.out.println("cc first update");
+		System.out.println("cc second update");
+
+		System.out.println("cc rebuild the file");
+	}
+}
+
